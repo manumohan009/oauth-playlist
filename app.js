8ab2aa3342ae350d7396ae63d43a54a69d36dbba
manumohan009 <manumohan.cvav@gmail.com>
2018-12-09 20:58:58 +0530

    OAuth (Passport.js) Tutorial #18 - Redirecting Users

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
+const profileRoutes = require('./routes/profile-routes');
 const passportSetup = require('./config/passport-setup');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
@@ -31,6 +32,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/auth',authRoutes);
+app.use('/profile',profileRoutes);
 
 app.listen(3000, () => {
     console.log('app now listening for requests on port 3000');
