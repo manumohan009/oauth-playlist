56209593164f02dbcef2152409c157dd25da16e6
manumohan009 <manumohan.cvav@gmail.com>
2018-12-09 18:10:50 +0530

    OAuth (Passport.js) Tutorial #7 - Securing Keys

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const authRoutes = require('./routes/auth-routes');
+
+const app = express();
+
+// set view engine
+app.set('view engine', 'ejs');
+
+// create home route
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.use('/auth',authRoutes);
+
+app.listen(3000, () => {
+    console.log('app now listening for requests on port 3000');
+});
