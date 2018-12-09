2ea70348e7adf33a092de027c81fb131d93a4f66
manumohan009 <manumohan.cvav@gmail.com>
2018-12-09 21:39:26 +0530

    OAuth (Passport.js) Tutorial #20 - Logging Users Out

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,19 +1,15 @@
 const router = require('express').Router();
 
-
 const authCheck = (req, res, next) => {
     if(!req.user){
-        // if user is not logged in
         res.redirect('/auth/login');
-    }else {
-        // if logged in 
+    } else {
         next();
     }
-}
+};
 
 router.get('/', authCheck, (req, res) => {
-    res.render('profile', {user: req.user});
+    res.render('profile', { user: req.user });
 });
 
 module.exports = router;
-
