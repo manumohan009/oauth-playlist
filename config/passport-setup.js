37b0d93687b87f2417735ad889528e4c07864a7e
manumohan009 <manumohan.cvav@gmail.com>
2018-12-09 20:08:30 +0530

    OAuth (Passport.js) Tutorial #13 - Retrieving Users

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -11,13 +11,20 @@ passport.use(
         clientSecret: keys.google.clientSecret
     },(accessToken, refreshToken, profile, done) => {
         // passport callback function
-        console.log('passport callback function fired');
-        console.log(profile);
-        new User({
-            username: profile.displayName,
-            googleId: profile.id
-        }).save().then((newUser) => {
-            console.log('new user created:' + newUser);
-        });
+        // check if user already exists in the db
+        User.findOne({googleId:profile.id }).then((currentUser) => {
+            if(currentUser) {
+                // already have the user
+                console.log('user is: ',currentUser );
+            } else {
+                // if not , create user in our db
+                new User({
+                    username: profile.displayName,
+                    googleId: profile.id
+                }).save().then((newUser) => {
+                    console.log('new user created:' + newUser);
+                });
+            }
+        })
     })
 )
